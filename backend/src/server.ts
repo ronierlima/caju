789ec8992a9fa82b335d7da64a098c009d579cd5
Ronier <ronier.lim@gmail.com>
2020-11-26 22:42:44 -0300

    Adcionando CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,6 @@ database.sync();
 console.log('Sever: ... Database running at 3306');
 
 
-app.listen(3000);
-console.log('Sever: ... Serve running at 3000');
+app.listen(3001);
+console.log('Sever: ... Serve running at 3001');
 
